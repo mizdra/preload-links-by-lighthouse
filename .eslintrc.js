1e130304a9b0bc35559da4dacb97e817c251bb2a
mizdra <pp.mizdra@gmail.com>
2021-02-05 00:00:48 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+/** @type import('eslint').Linter.BaseConfig */
+module.exports = {
+  root: true,
+  extends: [
+    // basic
+    '@mizdra/mizdra',
+    '@mizdra/mizdra/+typescript',
+    '@mizdra/mizdra/+prettier',
+  ],
+  env: {
+    node: true,
+  },
+  rules: {
+    'prettier/prettier': 0,
+  },
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# preload-link-generator
+
+Generate the template with embedded preload links using puppeteer
+
+## How to dev
+
+- `yarn run start`: Run for production
+- `yarn run build`: Build for production
+- `yarn run dev`: Run for development
+- `yarn run check`: Try static-checking
