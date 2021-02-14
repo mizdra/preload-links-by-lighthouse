dac24c3548832fc2b337314133425dd6143d53b9
mizdra <pp.mizdra@gmail.com>
2021-02-15 01:44:37 +0900

    example: fix type error

--- example/2-audit-in-parallel.ts
+++ example/2-audit-in-parallel.ts
@@ -1,8 +1,7 @@
 // usage: yarn run ts-node --files example/2-audit-in-parallel.ts
 
 import { resolve } from 'path';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import Piscina = require('piscina');
+import Piscina from 'piscina';
 import { filterByScript, generatePreloadLinks, NetworkRecord } from '../src/index';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
