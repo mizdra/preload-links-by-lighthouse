a6b39b1bf64b20f3ae3c3a12bf07388c442ddc37
mizdra <pp.mizdra@gmail.com>
2021-02-14 17:21:35 +0900

    feat!: expose Node.js API
    BREAKING CHANGE: this tool will no longer be a CLI tool

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-import { auditNetworkRequest } from './lib/lighthouse';
-import { filterByScript } from './lib/network-record-filter';
-import { generatePreloadLinks } from './lib/preload-link-generator';
-
-const url = process.argv[2];
-
-if (url === undefined) {
-  console.error('URL が渡されていません');
-  console.error('preload-link-generator <URL>');
-  process.exit(1);
-}
-
-(async () => {
-  const networkRecords = await auditNetworkRequest(url);
-  const filteredNetworkRecords = networkRecords.filter(filterByScript);
-  const html = generatePreloadLinks({ networkRecords: filteredNetworkRecords });
-  console.log(html);
-})().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+export * from './lib/lighthouse';
+export * from './lib/network-record-filter';
+export * from './lib/preload-link-generator';
